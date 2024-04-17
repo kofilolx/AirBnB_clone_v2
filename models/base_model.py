875ac8b31a2f601fd48b970ef0e38b767d274eef
kofilolx <kofyamando@gmail.com>
2024-04-17 14:48:49 -0700

    Buggy

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
-import uuid
 from datetime import datetime
-from sqlalchemy import Column, String, DateTime
+from sqlalchemy import Column, String, DATETIME
 from sqlalchemy.ext.declarative import declarative_base
+import os
+import uuid
 
 Base = declarative_base()
 
@@ -15,25 +16,25 @@ class BaseModel:
     updated_at = Column(DATETIME, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new BaseModel"""
-        if kwargs:
+        """Instantiates a new model"""
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key in ('created_at', 'updated_at'):
                         setattr(self, key, datetime.fromisoformat(value))
                     else:
                         setattr(self, key, value)
-            if 'id' not in kwargs:
-                self.id = str(uuid.uuid4())
-            if 'created_at' not in kwargs:
-                self.created_at = datetime.now()
-            if 'updated_at' not in kwargs:
-                self.updated_at = datetime.now()
-        else:
-            self.id = str(uuid.uuid4()
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            
+            if not hasattr(kwargs, 'id'):
+                setattr(self, 'id', str(uuid.uuid4()))
+            if not hasattr(kwargs, 'created_at'):
+                setattr(self, 'created_at', datetime.now())
+            if not hasattr(kwargs, 'updated_at'):
+                setattr(self, 'updated_at', datetime.now())
+
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
@@ -47,19 +48,18 @@ class BaseModel:
         storage.save()
 
     def to_dict(self):
-        """Creates a dictionary representation of the class instance.
-        Returns:
-            A dictionary containing all key-value pairs in __dict__.
-        """
-        instance_dict = dict(self.__dict__)
-        instance_dict["__class__"] = str(type(self).__name__)
-        instance_dict["created_at"] = self.created_at.isoformat()
-        instance_dict["updated_at"] = self.updated_at.isoformat()
-        if '_sa_instance_state' in instance_dict.keys():
-            del instance_dict['_sa_instance_state']
+        """Convert instance into dict format"""
+        instance_dict = {}
+        for key, value in self.__dict__.items():
+            if key != '_sa_instance_state':
+                if isinstance(value, datetime):
+                    instance_dict[key] = value.isoformat()
+                else:
+                    instance_dict[key] = value
+        instance_dict['__class__'] = self.__class__.__name__
         return instance_dict
 
     def delete(self):
-        """Deletes this basemodel instance from the storage"""
+        """Deletes this BaseModel instance from the storage"""
         from models import storage
         storage.delete(self)
