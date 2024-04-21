0559566632486b65305b19ca0d8323f5e631a311
kofilolx <kofyamando@gmail.com>
2024-04-20 23:43:53 -0400

    HBNB:console init

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-import os
-from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
+"""Create a unique storage instance for your application"""
 
+from os import environ
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
-storage = DBStorage() if os.getenv(
-        'HBNB_TYPE_STORAGE') == 'db' else FileStorage()
-"""
-A unique Filestorage/DBStorage instanace for all models
-"""
-storage.reload()
+# check envirn var to determine storage method
+if environ['HBNB_TYPE_STORAGE'] == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
+
+else:  # file storage selected
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
